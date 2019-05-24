a657ee102ca7a15eb506b67cce425de242430b2e
Stuart <stupop010@hotmail.co.uk>
2019-05-24 12:20:36 +0100

    scrape

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./config/db");
+
+connectDB();
 
 require("./models/User");
 require("./models/Job");
 
 // Cron jobs
-// require("./cronJob/cronScrap");
+require("./cronJob/cronScrap");
 require("./cronJob/cronEmail");
 
 app.use(cors());
@@ -18,7 +21,14 @@ app.use(express.json({ extended: false }));
 app.use("/auth", require("./routes/api/auth"));
 app.use("/api", require("./routes/api/jobs"));
 
-connectDB();
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5001;
 
